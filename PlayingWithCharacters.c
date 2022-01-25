b98d021bd76a2ffa912225e4330faae022f7a1d0
Nityoday Tekchandani <32616539+nityoday@users.noreply.github.com>
2022-01-26 00:01:00 +0530

    Added Code

---
+++ PlayingWithCharacters.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <string.h>
+int main()
+{
+    char ch;
+    char s[100],sen[100];
+    scanf("%c", &ch);
+    scanf("%s",s);
+    scanf("\n");
+    scanf("%[^\n]%*c", sen);
+    printf("%c\n%s\n%s", ch, s, sen);
+    return 0;
+}
